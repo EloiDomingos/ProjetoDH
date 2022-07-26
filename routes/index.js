32e922c9c64378a24445a05fe0c96a9ff5963c83
CarolZNS <nunes.carolzs@gmail.com>
2022-07-25 21:11:57 -0300

    Adiciona arquivo de rotas e de controller como modelo

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+//const controller = require(); //
+//modelo pra adicionar os controllers//
+
+router.get('/home')
+
+module.exports = router;
