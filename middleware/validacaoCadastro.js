af2af8e61bdd5282cfedc477e04bf0d484a6e16f
EloiDomingos <eloi.domingos@hotmail.com>
2022-07-29 21:30:15 -0300

    Inserindo middleware

---
+++ middleware/validacaoCadastro.js
@@ -0,0 +1,11 @@
+function validacaoCadastro(res, req, next){
+    var cad = req.body
+
+    if (cad.NomeCompleto == '' && cad.DataNascimento == ''
+    && cad.CadPessoaFisica == '' && cad.Email == '' && cad.SenhaCad){
+    res.status(403).send("Preencha todos os dados");
+    }else{
+        next
+    }
+
+}
