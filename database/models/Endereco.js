c61f2e37bf33423a9cc730bf320ecd119fb136e8
RobsonFranco89 <robsonfranco89.rf@gmail.com>
2022-08-28 18:30:55 -0300

    criado model Endereco testeDb rodando com banco de dados escola

---
+++ database/models/Endereco.js
@@ -0,0 +1,14 @@
+//const { DataTypes } = require("sequelize/types");
+
+module.exports = (sequelize, DataTypes) => {
+    const endereco = sequelize.define("Endereco", {
+        endereco: DataTypes.STRING,
+        cep: DataTypes.STRING,
+        cidade: DataTypes.STRING,
+    },
+    {
+        tableName: 'endereco',
+        timestamps: false
+    })
+    return endereco
+}
--- testeDb.js
+++ testeDb.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 const dbConfig = require('./database/config/config.json');
 const dbConn = new Sequelize(dbConfig.development);
 
-dbConn.query("select * from .livros", Sequelize.QueryTypes.SELECT).then(
+dbConn.query("select * from alunos", Sequelize.QueryTypes.SELECT).then(
     data=> {
         console.log(data);
         dbConn.close();
