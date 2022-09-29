a991b083ed2691a5dd0d528f2c7619c1bb480ed1
CarolZNS <nunes.carolzs@gmail.com>
2022-09-29 15:19:36 -0300

    comenta o codigo do arquivo

--- tt.js
+++ tt.js
@@ -1,53 +1,54 @@
-const { Produto } = require('./database/models');
-const beerList = [{
-    id: 1,
-    name: 'Heineken',
-    price: 15,
-    img: "/imagens/beer01.jpg"
-},
-{
-    id: 2,
-    name: 'Colorado',
-    price: 20,
-    img: "/imagens/cerveja-colorado-appia-600ml_77132.jpg"
-},
-{
-    id: 3,
-    name: 'Heineken',
-    price: 15,
-    img: "/imagens/beer01.jpg"
-},
-{
-    id: 4,
-    name: 'Colorado',
-    price: 20,
-    img: "/imagens/cerveja-colorado-appia-600ml_77132.jpg"
-},
-{
-    id: 5,
-    name: 'Heineken',
-    price: 15,
-    img: "/imagens/beer01.jpg"
-},
-{
-    id: 6,
-    name: 'Colorado',
-    price: 20,
-    img: "/imagens/cerveja-colorado-appia-600ml_77132.jpg"
-}]
-async function criandoProduto(){
-    for (let index = 0; index < beerList.length; index++) {
-        const element = beerList[index];
-        console.log(element)
-        const resultSave = await Produto.create({
-            nome:element.name,
-            preco:element.price,
-            tamanho: 500,
-            img:element.img,
-            marca: 'Devs',
-            tipo_id: 1
-        })
-    }
+//Favor ignorar este arquivo. Banco de dados já devidamente populado em outro arquivo
+// const { Produto } = require('./database/models');
+// const beerList = [{
+//     id: 1,
+//     name: 'Heineken',
+//     price: 15,
+//     img: "/imagens/beer01.jpg"
+// },
+// {
+//     id: 2,
+//     name: 'Colorado',
+//     price: 20,
+//     img: "/imagens/cerveja-colorado-appia-600ml_77132.jpg"
+// },
+// {
+//     id: 3,
+//     name: 'Heineken',
+//     price: 15,
+//     img: "/imagens/beer01.jpg"
+// },
+// {
+//     id: 4,
+//     name: 'Colorado',
+//     price: 20,
+//     img: "/imagens/cerveja-colorado-appia-600ml_77132.jpg"
+// },
+// {
+//     id: 5,
+//     name: 'Heineken',
+//     price: 15,
+//     img: "/imagens/beer01.jpg"
+// },
+// {
+//     id: 6,
+//     name: 'Colorado',
+//     price: 20,
+//     img: "/imagens/cerveja-colorado-appia-600ml_77132.jpg"
+// }]
+// async function criandoProduto(){
+//     for (let index = 0; index < beerList.length; index++) {
+//         const element = beerList[index];
+//         console.log(element)
+//         const resultSave = await Produto.create({
+//             nome:element.name,
+//             preco:element.price,
+//             tamanho: 500,
+//             img:element.img,
+//             marca: 'Devs',
+//             tipo_id: 1
+//         })
+//     }
 
-};
-criandoProduto();
+// };
+// criandoProduto();
